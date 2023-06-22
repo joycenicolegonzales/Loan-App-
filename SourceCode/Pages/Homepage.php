bc76e3e061b865f8e3d815254089c9f6b33d9a10
efrenmiguelpelo <efrenmiguelpelo@gmail.com>
2023-06-22 14:16:47 +0800

    test push

--- SourceCode/Pages/Homepage.php
+++ SourceCode/Pages/Homepage.php
@@ -6,18 +6,15 @@
     <title>Document</title>
  
     <!-- <link rel="stylesheet" href="/sdasdasd"> -->
-    <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/bootstrap.css"/>
-    <!-- <link rel="stylesheet" href="../bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"/> -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
-  </head> 
+    <link rel="stylesheet" href="../bootstrap-5.2.2\css\bootstrap.css"/>
+</head> 
 
 <body>
-    <nav class="navbar navbar-expand-lg " style="background-color: #1165AE">
-        <div class="container-fluid ">
-          <a class="navbar-brand d-flex " href="#"><img src="/SourceCode/images/logoo.jpg" alt="" class="ms-1" style="width: 53px; height: 53px;"> <h3 class="mt-2 ms-1" style="color: #ffe83a;">NMEMPC</h3> </a>
-          <button class="navbar-toggler"  style="color: black;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon "></span>
+    <nav class="navbar navbar-expand-lg navbar-light" style="">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="/" alt=""></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -42,10 +39,10 @@
                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
               </li>
             </ul>
-            <!-- <form class="d-flex">
+            <form class="d-flex">
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
               <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> -->
+            </form>
           </div>
         </div>
       </nav>
