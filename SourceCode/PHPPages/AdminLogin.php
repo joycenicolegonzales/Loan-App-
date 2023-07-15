458cbf918b01deb719dffeb990c17f295914ccc7
joycenicolegonzales <gonzalesjoycenicole@gmail.com>
2023-07-15 14:55:31 +0800

    admin login php

--- SourceCode/PHPPages/AdminLogin.php
+++ SourceCode/PHPPages/AdminLogin.php
@@ -1,10 +1,13 @@
-<?php 
-   session_start();
-   if (!isset($_SESSION['username']) && !isset($_SESSION['id'])) {   ?>
+<?php
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>multi-user role-based-login-system</title>
+	<title>AdminStaff Login</title>
+	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <link rel="stylesheet" href="../bootstrap-5.2.2/css/bootstrap.min.css"/>
@@ -20,33 +23,39 @@
 	  
 </head>
 <body>
-      <div class="container d-flex justify-content-center align-items-center"
+		<div class="container d-flex justify-content-center align-items-center"
       style="min-height: 100vh">
+				<?php
+
+				error_reporting(0);
+				session_start();
+				session_destroy();
+
+				echo $_SESSION['loginMessage'];
+				
+				?>
+
       	<form class="border shadow p-3 rounded"
-      	      action="php/check-login.php" 
+      	      action="database.php" 
       	      method="post" 
       	      style="width: 450px;">
       	      <h1 class="text-center p-3">LOGIN</h1>
-      	      <?php if (isset($_GET['error'])) { ?>
-      	      <div class="alert alert-danger" role="alert">
-				  <?=$_GET['error']?>
-			  </div>
-			  <?php } ?>
+      	    
 		  <div class="mb-3">
 		    <label for="username" 
 		           class="form-label">User name</label>
 		    <input type="text" 
 		           class="form-control" 
 		           name="username" 
-		           id="username">
+		          >
+				  
 		  </div>
 		  <div class="mb-3">
 		    <label for="password" 
 		           class="form-label">Password</label>
 		    <input type="password" 
 		           name="password" 
-		           class="form-control" 
-		           id="password">
+		           class="form-control"  >
 		  </div>
 		  <div class="mb-1">
 		    <label class="form-label">Select User Type:</label>
@@ -54,16 +63,16 @@
 		  <select class="form-select mb-3"
 		          name="role" 
 		          aria-label="Default select example">
-			  <option selected value="user">User</option>
-			  <option value="admin">Admin</option>
+			  <option selected value="admin">Admin</option>
+			  <option value="user">User</option>
 		  </select>
 		 
 		  <button type="submit" 
-		          class="btn btn-primary">LOGIN</button>
-		</form>
-      </div>
+		          name="submit" class="btn btn-primary">LOGIN</button>
+			</form>     
+	</div>
+			
+ 
 </body>
 </html>
-<?php }else{
-	header("Location: home.php");
-} ?>
+
