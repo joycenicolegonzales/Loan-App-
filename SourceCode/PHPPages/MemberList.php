7b0fd46b4d47e27434637b9b2343bf2ad50e8d98
efrenmiguelpelo <efrenmiguelpelo@gmail.com>
2023-07-17 17:23:22 +0800

    btn

--- SourceCode/PHPPages/MemberList.php
+++ SourceCode/PHPPages/MemberList.php
@@ -985,8 +985,10 @@ if(isset($_POST['submit']))
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <!-- Bootstrap 5 JS CDN Links -->
-   <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script> -->
+<!-- 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/js/jquery.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.min.js/1.14.6/umd/popper.min.js"></script>  -->
+
 
 
     <script>
